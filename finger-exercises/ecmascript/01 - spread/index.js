134a99dbcdb9e62fe722441395cf1e6ad1703e00
Alejo Aquili <aaquili@itba.edu.ar>
2019-03-14 10:55:38 -0300

    min params test working

--- finger-exercises/ecmascript/01 - spread/index.js
+++ finger-exercises/ecmascript/01 - spread/index.js
@@ -1,8 +1,8 @@
 import { isArray } from './utils';
 
-export function min() {
-  isArray();
-  return Math.min();
+export function min(...num) {
+  //isArray();
+  return Math.min(...num);
 }
 
 export function copy() {
