ff7e37fce5df11b8b6a0bade8b56df8919d61e63
Alejo Aquili <aaquili@itba.edu.ar>
2019-03-15 09:48:34 -0300

    promise in delay function tests fixed

--- finger-exercises/ecmascript/02 - promises/index.js
+++ finger-exercises/ecmascript/02 - promises/index.js
@@ -1,7 +1,14 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
-
-export function delay() {
-
+const MAX_TIME = 500 * 500;
+export function delay(time) {
+  return new Promise(function(resolve, reject) {
+    if (time >= MAX_TIME) {
+      reject(new Error('This time is too much !'));
+    }
+    setTimeout(function() {
+      resolve(time);
+  }, time)
+  });
 }
 
 export function asyncDelay() {
