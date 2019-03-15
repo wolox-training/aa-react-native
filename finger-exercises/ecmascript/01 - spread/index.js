53db96058e6267dea9454703e2c98055c2eeaea8
Alejo Aquili <aaquili@itba.edu.ar>
2019-03-15 08:29:46 -0300

    no argument to min function test fixed

--- finger-exercises/ecmascript/01 - spread/index.js
+++ finger-exercises/ecmascript/01 - spread/index.js
@@ -1,6 +1,9 @@
 import { isArray } from './utils';
 
 export function min(...num) {
+  if (num.length === 0) {
+    return undefined;
+  }
   if (isArray(...num)) {
     return Math.min.apply(null, ...num);
   }
