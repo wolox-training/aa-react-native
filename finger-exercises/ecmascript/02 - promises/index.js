8bfa4c992ab5b8e55d5b31f2a354838d5c2c8ec3
Alejo Aquili <aaquili@itba.edu.ar>
2019-03-15 11:17:58 -0300

    async await deñay function test fixed

--- finger-exercises/ecmascript/02 - promises/index.js
+++ finger-exercises/ecmascript/02 - promises/index.js
@@ -1,16 +1,15 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 const MAX_TIME = 500 * 500;
 export function delay(time) {
-  return new Promise(function(resolve, reject) {
+  return new Promise((resolve, reject) => {
     if (time >= MAX_TIME) {
       reject(new Error('This time is too much !'));
     }
-    setTimeout(function() {
-      resolve(time);
-  }, time)
+    setTimeout(() => resolve(time), time);
   });
 }
 
-export function asyncDelay() {
-
+export async function asyncDelay(time) {
+  await setTimeout(time);
+  return time;
 }
