505c10c62431e8fe841f5748e09f1157fbe3cd6a
Alejo Aquili <aaquili@itba.edu.ar>
2019-03-28 08:20:09 -0300

    fixing scroll

--- src/app/screens/BookDetail/components/CommentList/styles.js
+++ src/app/screens/BookDetail/components/CommentList/styles.js
@@ -6,7 +6,6 @@ import { SMALL } from '../../../../../constants/fontSizes';
 import { skyBlue } from '../../../../../constants/colors';
 export default StyleSheet.create({
     container: {
-        flex: 1,
         marginHorizontal:SPACING_MEDIUM,
         backgroundColor: white,
         borderRadius: RADIUS_SMALL
