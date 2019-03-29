e77fe6aad4233ce0f4c7d6218c44eaab8da03e3e
Alejo Aquili <aaquili@itba.edu.ar>
2019-03-29 13:39:09 -0300

    tab navigator

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createStackNavigator, createAppContainer, createBottomTabNavigator } from 'react-navigation';
 
 import BookList from './screens/BookList';
 import BookDetail from './screens/BookDetail';
 
-
 import * as Routes from '../constants/routes';
 import { defaultNavigationOptions } from '../config/navigationOptions';
 
+// TODO: remove Dummy is just for test.
+
+const TabNavigator = createBottomTabNavigator({
+  [Routes.BookList]: {
+    screen: BookList
+  },
+  [Routes.Dummy]: {
+    screen: BookList
+  }
+});
 
 const AppNavigator = createStackNavigator({
     [Routes.BookList]: {
--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,3 +1,3 @@
 export const BookList = 'BookList';
 export const BookDetail = 'BookDetail';
-
+export const Dummy = 'Dummy';
