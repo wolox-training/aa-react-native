193e12efd6c257b9be6187e009701435d9d64fa3
Alejo Aquili <aaquili@itba.edu.ar>
2019-03-29 13:19:31 -0300

    some changes

--- src/app/components/BackButton/index.js
+++ src/app/components/BackButton/index.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import { Image, TouchableOpacity } from 'react-native';
 import { withNavigation } from 'react-navigation';
 
 import styles from './styles';
 import backImage from '../../assets/ic_back.png';
 
-class BackButton extends Component {
+class BackButton extends PureComponent {
+  handleGoBack = () => this.props.navigation.goBack();
   render() {
     return (
-      <TouchableOpacity style={styles.container} onPress={() => { this.props.navigation.goBack()}}>
+      <TouchableOpacity style={styles.container} onPress={this.handleGoBack}>
         <Image style={styles.image} source={backImage} resizeMode="stretch"/>
       </TouchableOpacity>
     )
