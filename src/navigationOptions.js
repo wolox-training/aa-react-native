7eca8178df7ac294b50869a7675359ca1eef71d7
Alejo Aquili <aaquili@itba.edu.ar>
2019-03-29 13:04:38 -0300

    nav options

--- src/navigationOptions.js
+++ src/navigationOptions.js
@@ -1,11 +1,11 @@
-import HeaderBackground from './components/HeaderBackground';
-import { HEADER_HEIGHT } from '../constants/platform';
-import { white } from '../constants/colors';
-import { MEDIUM } from '../constants/fontSizes';
-import { SPACING_BIG } from '../constants/dimensions';
+import React from 'react';
+import HeaderBackground from './app/components/HeaderBackground';
+import { HEADER_HEIGHT } from './constants/platform';
+import { white } from './constants/colors';
+import { MEDIUM, BOLD } from './constants/fontSizes';
 
 export const defaultNavigationOptions = {
     headerStyle: {height: HEADER_HEIGHT, width: '100%', backgroundColor: 'transparent'},
     headerBackground: <HeaderBackground/>,
-    headerTitleStyle: { color: white, fontSize: MEDIUM, fontWeight: "bold", marginLeft: SPACING_BIG}
+    headerTitleStyle: { color: white, fontSize: MEDIUM, fontWeight: BOLD}
 };
