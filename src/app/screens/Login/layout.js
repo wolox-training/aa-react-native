f27f08bac94048d404050a2ac7f5db0b097deb98
Alejo Aquili <aaquili@itba.edu.ar>
2019-04-09 09:57:31 -0300

    extra commonprops

--- src/app/screens/Login/layout.js
+++ src/app/screens/Login/layout.js
@@ -14,8 +14,6 @@ function Login() {
                     autoComplete='email'
                     keyboardType='email-address'
                     textContentType='emailAddress'
-                    commonProps
-
                 />
                 <TextInput
                     {...commonProps}
