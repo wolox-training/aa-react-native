a9e0ad1cc586fed82783ec86113b50ede4fe44d6
Alejo Aquili <aaquili@itba.edu.ar>
2019-04-11 11:40:42 -0300

    storage in service

--- src/redux/auth/actions.js
+++ src/redux/auth/actions.js
@@ -1,4 +1,3 @@
-import AsyncStorage from '@react-native-community/async-storage';
 import AuthService from '../../services/AuthService';
 export const actions = {
     SIGN_IN: '@@AUTH/SIGN_IN',
@@ -6,26 +5,6 @@ export const actions = {
     SIGN_IN_FAILURE: '@@AUTH/SIGN_IN_FAILURE'
   };
   
-
-const storeData = async (key, value) => {
-    try {
-      await AsyncStorage.setItem(key, value)
-    } catch (e) {
-      // saving error
-    }
-}
-
-
-const getData = async () => {
-    try {
-      const value = await AsyncStorage.getItem('@storage_Key')
-      if(value !== null) {
-        // value previously stored
-      }
-    } catch(e) {
-      // error reading value
-    }
-}
 
 const actionsCreators = {
     signIn: (email, password) => async dispatch => {
@@ -36,7 +15,6 @@ const actionsCreators = {
                 type: actions.SIGN_IN_SUCCESS,
                 payload: response.data
             });
-            storeData('@AccessToken', response.headers.Access-Token);
         } else {
             dispatch({
                 type: actions.SIGN_IN_FAILURE,
--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,7 +1,42 @@
+import AsyncStorage from '@react-native-community/async-storage';
 import api from '../config/api';
 
 const ENDPOINT = '/auth';
 
+
+const storeData = async (key, value) => {
+  try {
+    await AsyncStorage.setItem(key, value)
+  } catch (e) {
+    // saving error
+  }
+}
+
+
+const getData = async (key) => {
+  try {
+    return await AsyncStorage.getItem(key)
+
+  } catch(e) {
+    // error reading value
+  }
+}
+
 export default {
-  signIn: (email, password) => api.post(`${ENDPOINT}/sign_in`, { email, password })
+  signIn: (email, password) => {
+    const response = api.post(`${ENDPOINT}/sign_in`, { email, password });
+    if (response.ok) {
+      storeData('@Auth:currentUser', response.data);
+      storeData('@Auth:accessToken', response.headers.AccessToken);
+      storeData('@Auth:client', response.headers.client);
+
+      api.setHeaders({
+        'Acces-Token': response.headers.AccessToken,
+        Client: response.headers.Client,
+        Uid: response.headers.Uid
+      });
+    }
+    return response;
+  },
+  isSignIn: async () => await getData('@Auth:currentUser')
 };
