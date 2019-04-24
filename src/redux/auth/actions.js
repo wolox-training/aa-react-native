8192ea672830de1643e8aabbb5fab98f6b0e4482
Alejo Aquili <aaquili@itba.edu.ar>
2019-04-24 12:11:49 -0300

    delete comments in code

--- src/redux/auth/actions.js
+++ src/redux/auth/actions.js
@@ -5,34 +5,6 @@ const signInTarget = 'currentUser';
 
 export const actions = createTypes(completeTypes(['SIGN_IN'], ['AUTH_INIT']), '@@AUTH');
 
-/*export const  actions = {
-    SIGN_IN:'@@AUTH/SIGN_IN',
-    SIGN_IN_SUCCESS: '@@AUTH/SIGN_IN_SUCCESS',
-    SIGN_IN_FAILURE: '@@AUTH/SIGN_IN_FAILURE',
-    AUTH_INIT: '@@AUTH/AUTH_INIT'
-  };
-  
-
-const actionsCreators = {
-    signIn: (email, password) => async dispatch => {
-        dispatch({type: actions.SIGN_IN});
-        const response = await AuthService.signIn(email, password);
-        if(response.ok) {
-            await AuthService.setCurrentUser(response.data, response.headers);
-            dispatch({
-                type: actions.SIGN_IN_SUCCESS,
-                payload: response.data
-            });
-        } else {
-            dispatch({
-                type: actions.SIGN_IN_FAILURE,
-                payload: response.data.errors[0]
-            });
-        }
-    },
-    authInit: (user) => ({ type: actions.AUTH_INIT, payload: user })
-};*/
-
 const actionsCreators = {
     init: user => ({ type: actions.AUTH_INIT, target: signInTarget, payload: user }),
     signIn: (email, password) => ({
--- src/redux/auth/reducer.js
+++ src/redux/auth/reducer.js
@@ -6,30 +6,6 @@ const stateDescription = {
   initialLoading: true
 };
 
-/*
-const initialState = {
-  currentUser: null,
-  signInErrorMessage: null,
-  isLoading: false,
-  initialLoading: true
-};
-
-function reducer(state = initialState, action) {
-  switch (action.type) {
-    case actions.SIGN_IN: 
-      return {...state, isLoading: true};
-    case actions.SIGN_IN_SUCCESS: 
-      return {...state, currentUser: action.payload, signInErrorMessage: null, isLoading: false};
-    case actions.SIGN_IN_FAILURE: 
-      return {...state, signInErrorMessage: action.payload, isLoading: false};
-    case actions.AUTH_INIT:
-      return {...state, currentUser: action.payload, initialLoading: false};
-    default:
-      return state;
-  }
-}
-*/
-
 const initialState = completeState(stateDescription, ['initialLoading']);
 
 const reducerDescription = {
