1c6879f2656f2688c8565a0ab751aaf102436bcb
Alejo Aquili <aaquili@itba.edu.ar>
2019-05-10 09:04:27 -0300

    some changes

--- src/app/screens/BookDetail/components/BookDetail/index.js
+++ src/app/screens/BookDetail/components/BookDetail/index.js
@@ -28,39 +28,38 @@ class BookDetail extends Component{
     };
 
     render() {
-    const { title, author, imageSource, year, type } = this.props;
-    const aux = [author, year, type];
-    const { isRented, animated } = this.state;
-    return (
-        <View style={styles.container}>
-            <View style={styles.bookContainer}>
-                <Image style={styles.image} resizeMode="cover" source={imageSource||defaultImage}></Image>
-                <View style={styles.infoContainer}>
-                    <Text style={styles.title} numberOfLines={1}>{title}</Text>
-                    <View style={styles.dataContainer}>
-                        {aux.map(value => <Text key={value} style={styles.text}>{value}</Text>)}
+        const { title, author, imageSource, year, type } = this.props;
+        const aux = [author, year, type];
+        const { isRented, animated } = this.state;
+        return (
+            <View style={styles.container}>
+                <View style={styles.bookContainer}>
+                    <Image style={styles.image} resizeMode="cover" source={imageSource||defaultImage}></Image>
+                    <View style={styles.infoContainer}>
+                        <Text style={styles.title} numberOfLines={1}>{title}</Text>
+                        <View style={styles.dataContainer}>
+                            {aux.map(value => <Text key={value} style={styles.text}>{value}</Text>)}
+                        </View>
                     </View>
                 </View>
+                <View style={styles.buttonsContainer}>
+                    <TouchableOpacity style={styles.addToWishList} >
+                        <Text style={styles.addToWishListText}>ADD TO WISH LIST</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={this.handleOnPressRent}>
+                        {!isRented && (
+                        <LinearGradient colors={[skyBlue, cian]}  start={{x: 0, y: 0}} end={{x: 1, y: 0}} style={styles.button}>
+                            <Text style={styles.rentText}>RENT</Text>
+                        </LinearGradient>)}
+                        {isRented && (
+                            <Animated.View style={[styles.button, styles.animatedRentButton, { height: animated, maxWidth: animated}]}>
+                                <Icon name='md-checkmark' type='ionicon' color={ white }/>
+                            </Animated.View>)}
+                    </TouchableOpacity>
+                </View>
             </View>
-            <View style={styles.buttonsContainer}>
-                <TouchableOpacity style={styles.addToWishList} >
-                    <Text style={styles.addToWishListText}>ADD TO WISH LIST</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={[styles.button]} onPress={this.handleOnPressRent}>
-                    {!isRented && (
-                    <LinearGradient colors={[skyBlue, cian]}  start={{x: 0, y: 0}} end={{x: 1, y: 0}} style={[styles.button]}>
-                        <Text style={styles.rentText}>RENT</Text>
-                    </LinearGradient>)}
-
-                    {isRented && (
-                        <Animated.View style={[styles.button, styles.animatedRentButton, { height: animated, maxWidth: animated}]}>
-                            <Icon name='md-checkmark' type='ionicon' color={ white }/>
-                        </Animated.View>)}
-                </TouchableOpacity>
-            </View>
-        </View>
-    );
-}
+        );
+    }   
 }
 
 export default BookDetail;
